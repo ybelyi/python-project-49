1a84ef6c6f247d7abf4743b4d78b3b638c499ee8
Yury Belyi <ybelyi@gmail.com>
2024-05-19 10:30:09 +0300

    Stage1 finished

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,13 @@
 
-print('Welcome to the Brain Games!')
+def greet():
+    print("Welcome to the Brain Games!")
+
+
+def main():
+
+    greet()
+
 
 if __name__ == '__main__':
 
-    main()
+        main()
